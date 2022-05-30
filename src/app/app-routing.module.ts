bcc737465bc21ea9274a75c17e475fbc03dffba7
JoanOceguera <joan.oceguera.napoles@gmail.com>
2022-05-29 22:28:47 -0400

    - Arreglado error en app.component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RendimientoComercialComponent } from './rendimiento-comercial/rendimiento-comercial.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: RendimientoComercialComponent},
+  {path:'rendimiento-comercial', component: RendimientoComercialComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
