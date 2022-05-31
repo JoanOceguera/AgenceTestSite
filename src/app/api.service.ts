bad880c32790969c76e84f375e09e28dbec25930
JoanOceguera <joan.oceguera.napoles@gmail.com>
2022-05-31 13:18:57 -0400

    - Actualizada la api-service para obtener los datos desde la api publica

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,29 +1,31 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams} from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Moment } from 'moment';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ApiService {
+export class ApiService
+{
 
   constructor(private http: HttpClient) { }
 
-  baseUrl: string = "https://localhost:44332/api/Adviser/";
-  
+  baseUrl: string = "http://joanoceguera-001-site1.btempurl.com/api/Adviser/";
+
   getConsultores ()
   {
     return this.http.get(this.baseUrl);
   }
 
-  getResumenConsultores (consultores: string [], fechaInicio: Moment, fechaFin: Moment)
+  getResumenConsultores (consultores: string[], fechaInicio: Moment, fechaFin: Moment)
   {
     let params = new HttpParams()
       .set('begin', fechaInicio.toISOString())
       .set('end', fechaFin.toISOString());
-    consultores.forEach((nombre:string) =>{
-  params = params.append('advicers', nombre);
-})
-    return this.http.get(this.baseUrl + "GetAdvisersSummary", {params});
+    consultores.forEach((nombre: string) =>
+    {
+      params = params.append('advicers', nombre);
+    });
+    return this.http.get(this.baseUrl + "GetAdvisersSummary", { params });
   }
 }
